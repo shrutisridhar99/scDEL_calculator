338fe38065398bdd2300fd1a8f0f1381c17a4c9b
Kanav Gupta <kakarrot.goku2003@gmail.com>
2024-10-16 16:31:21 +0800

    clean

--- main.py
+++ main.py
@@ -142,6 +142,8 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            #GithubIcon {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
